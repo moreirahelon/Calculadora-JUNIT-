cbfd3366384b4e5f3f58943a49f99ed5da53a271
moreirahelon <moreirahelon@gmail.com>
2016-05-15 13:08:49 -0300

    primeiros testes

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+package Calculadora;
+
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+	public static void main(String[] args){
+		JanelaPrincipal obj = new JanelaPrincipal();
+		obj.show();
+	}
+}
